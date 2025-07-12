3209f448b4254e677dc21cdc82febc56e79dc1a4
Vipanshu Mittal <vipanshucareer@gmail.com>
2025-07-12 23:20:26 +0530

    Add files via upload

---
+++ emailjs-config.js
@@ -0,0 +1,5 @@
+// EmailJS configuration (do NOT commit this file to public repos)
+window.EMAILJS_PUBLIC_KEY = "eGCpkL53Krefr1K-V";
+window.EMAILJS_SERVICE_ID = "service_uobiro9";
+window.EMAILJS_TEMPLATE_ID = "template_6rheieg";
+window.EMAILJS_AUTOREPLY_TEMPLATE_ID = "template_csab1hd"; 
